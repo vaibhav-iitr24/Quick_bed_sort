00d91ca994e666e821777617aeebc2448e714229
vaibhav-iitr24 <vaibhav_ts@bt.iitr.ac.in>
2024-10-14 11:41:45 +0530

    Update qbs.sh

--- qbs.sh
+++ qbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gunzip shuf.a.bed shuf.b.bed
+gunzip shuf.a.bed.gz shuf.b.bed.gz
 cat shuf.a.bed shuf.b.bed | awk '{print > $1".bed"}'
 
 snakemake -p --snakefile segregate.smk Output/for_demo.bed 
