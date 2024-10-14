54b6506e2fdb6829119b61e4e97bbfc6d49583d1
vaibhav-iitr24 <vaibhav_ts@bt.iitr.ac.in>
2024-10-14 13:14:34 +0530

    Update trial_sorted.sh

--- trial_sorted.sh
+++ trial_sorted.sh
@@ -2,17 +2,9 @@
 # $1: list of input files separated by space
 # $2: output file
 
-echo "Input files: $1"
-echo "Output file: $2"
-
-> "$2"
-
 echo "$1" | tr ' ' '\n' | while read fname
 do
-    echo "Processing file: $fname"
-    sort -k2,2 -k3,3 "$fname" >> "$2"
-    echo "Sorted content from $fname appended to $2"
-done
+    sort -k2,2 -k3,3  $fname 
+done > "$2"
 
-echo "Final output written to $2"
 
